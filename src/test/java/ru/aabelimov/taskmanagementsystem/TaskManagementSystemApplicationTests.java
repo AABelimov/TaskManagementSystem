f6feff7026bdf4eb443aa676d9da77cb4eda855d
AABelimov <belimov98@gmail.com>
2025-03-04 15:54:49 +0300

    Initial commit

---
+++ src/test/java/ru/aabelimov/taskmanagementsystem/TaskManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aabelimov.taskmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
